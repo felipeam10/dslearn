abcb06a0ea6c60b3c92abec196eac3956657a6d4
felipeam10 <felipeam10@hotmail.com>
2024-03-06 01:20:20 -0300

    vJSR-1.12 - Enrollment and EnrollmentPK

---
+++ src/main/java/com/felipe/dslearn/entities/pk/EnrrolmentPK.java
@@ -0,0 +1,61 @@
+package com.felipe.dslearn.entities.pk;
+
+import com.felipe.dslearn.entities.Offer;
+import com.felipe.dslearn.entities.User;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Objects;
+
+@Embeddable
+public class EnrrolmentPK {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "offer_id")
+    private Offer offer;
+
+    public EnrrolmentPK(){}
+
+    public EnrrolmentPK(User user, Offer offer) {
+        this.user = user;
+        this.offer = offer;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Offer getOffer() {
+        return offer;
+    }
+
+    public void setOffer(Offer offer) {
+        this.offer = offer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EnrrolmentPK that = (EnrrolmentPK) o;
+
+        if (!Objects.equals(user, that.user)) return false;
+        return Objects.equals(offer, that.offer);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = user != null ? user.hashCode() : 0;
+        result = 31 * result + (offer != null ? offer.hashCode() : 0);
+        return result;
+    }
+}
